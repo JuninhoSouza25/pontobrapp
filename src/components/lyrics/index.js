29c37ca59dbf62e8d2f7fb76eb8734b33156be7d
JuninhoSouza25 <dnt-bh@hotmail.com>
2022-03-16 17:53:00 -0300

    mini

--- src/components/lyrics/index.js
+++ src/components/lyrics/index.js
@@ -2,27 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 export default function Lyrics(props){
-
     const [music, setMusic] = useState()
 
     useEffect(() => {
         axios.get(`https://api.vagalume.com.br/search.php?apikey=660a4395f992ff67786584e238f501aa&${props.props}`)
         .then((response) => {
-            
             setMusic(response.data.mus[0].text)
-            console.log(response.data.mus[0].text)
-            
         });
     }, []);
-
-    
 
     return(
         <div style={{color:"#fcf1d5", textAlign:"center"}}>
             {music}
         </div>
-    )
-
-    
+    ) 
 }
 
