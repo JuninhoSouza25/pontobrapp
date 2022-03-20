baf5c394d610f6aad5979daefe65283da045db80
JuninhoSouza25 <dnt-bh@hotmail.com>
2022-03-20 20:13:00 -0300

    Playlist

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../components/logo";
 import './home.css';  
-import { AiOutlineOrderedList, AiOutlineRead } from "react-icons/ai";
+import { AiOutlineOrderedList, AiOutlineRead, AiOutlineMenuUnfold } from "react-icons/ai";
 
 export function Home() {
 
@@ -13,6 +13,7 @@ export function Home() {
       <div className="home">
           <Link className="home-link" to='/repertorio'><AiOutlineOrderedList color="inherit" size={40} />Repertório</Link>
           <Link className="home-link" to='/agenda'><AiOutlineRead color="inherit" size={40} />Agenda</Link>
+          <Link className="home-link" to='/agenda'><AiOutlineMenuUnfold color="inherit" size={40} />Playlists</Link>
       </div>
     </>
   );
