f7be139b57573d658b2c56c1c00ad6ffec9c2b4a
JuninhoSouza25 <dnt-bh@hotmail.com>
2022-03-21 15:27:11 -0300

    Calendario

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -5,7 +5,6 @@ import './gallerycomponent.css'
 export default function GalleryComponent() {
     return(
         <div className="gallery-component--container">
-            
         </div>
     )
 }
--- src/pages/schedule/index.js
+++ src/pages/schedule/index.js
@@ -9,7 +9,7 @@ export default function Schedule(){
             <Logo />
             <div className="schedule-container">
                 <h1>Agenda</h1>
-
+                <iframe title="schedule" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23fcf1d5&ctz=America%2FSao_Paulo&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=YmFuZGFwb250b2JyMjVAZ21haWwuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=cHQtYnIuYnJhemlsaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043"  width="90%" height="550" frameborder="0" scrolling="no"></iframe>
             </div>
             <Menu />
         </>
